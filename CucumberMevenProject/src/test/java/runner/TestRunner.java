9434a7f66a88a61dd0083874a646f9b8e1ea292f
pavanysecit <pavan.kumar@ysecit.com>
2020-02-18 16:55:09 +0530

    comment

--- CucumberMevenProject/src/test/java/runner/TestRunner.java
+++ CucumberMevenProject/src/test/java/runner/TestRunner.java
@@ -3,16 +3,9 @@ package runner;
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-
+//comment
 @RunWith(Cucumber.class)
-@CucumberOptions(features=".//features/MyAttendanc.feature", 
-					glue={"stepDefinition"}, 					// ".//features/Myattendanc.fature"	
-					dryRun=false,								//cross checking every feature has steps
-					monochrome=true,							//remove unneceessay character from console.
-					plugin={"html:test-output"}      			//Generate Html Report
-					//tags={"@sanity, @segression"}  			OR //To run specific scenarion under steps definitions
-					//tags={"@sanity", "@segression"}  			AND //To run both santiy and regression test
-)
+@CucumberOptions(features="C:/Users/sw176/git/CucumberMevenProject/CucumberMevenProject/features/MyAttendanc.feature", glue={"stepDefinition"})
 public class TestRunner 
 {
 
