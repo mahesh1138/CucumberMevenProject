f7b4d95310d174682ea3458a5d4ceaf80a73b6ec
pavanysecit <pavan.kumar@ysecit.com>
2020-02-19 17:12:30 +0530

    f

--- CucumberMevenProject/src/test/java/runner/TestRunner.java
+++ CucumberMevenProject/src/test/java/runner/TestRunner.java
@@ -3,7 +3,7 @@ package runner;
 import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-//comment//
+//comment////
 @RunWith(Cucumber.class)
 @CucumberOptions(features="C:/Users/sw176/git/CucumberMevenProject/CucumberMevenProject/features/MyAttendanc.feature", glue={"stepDefinition"})
 public class TestRunner 
